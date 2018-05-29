0125fe928f0bb256fbfb138c6681bf6bb7c02129
Marie Cain <mariecain27@gmail.com>
2018-05-29 03:23:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,15 @@ def countdown(number)
   "HAPPY NEW YEAR!"
   
 end
+
+
+def countdown(number)
+  
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -=1
+  end
+  
+  "HAPPY NEW YEAR!"
+  
+end
